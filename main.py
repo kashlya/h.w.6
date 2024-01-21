69b951059e200f8e480fec4f69a6515654c7eff8
38096 <05lozaverlan@gmail.com>
2024-01-21 13:01:48 +0100

    version 1.2

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-for i in range(1, 11):
-    for j in range(1, 11):
-        ravno = i * j
-        print(f"{i} * {j} = {ravno}", end="\t")
-    print("\n")
+nachalo = int(input("начало- "))
+konec = int(input("конец- "))
+for num in range(max(2, nachalo), konec + 1):
+    for i in range(max(2, num):
+        if num % i == 0:
+            break
+    else:
+        print(num, end=" ")
