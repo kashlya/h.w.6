3309a15c857748387dcc4d5e871bc79bb7fb0bc1
38096 <05lozaverlan@gmail.com>
2024-01-21 12:48:21 +0100

    version 1.1

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(3, 6):
-    for j in range(3, 6):
+for i in range(1, 11):
+    for j in range(1, 11):
         ravno = i * j
         print(f"{i} * {j} = {ravno}", end="\t")
-    print("\n" + "-----------------------------------")
+    print("\n")
